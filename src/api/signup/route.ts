824a475bea67280fb0fbc0e18ddd61d7f97706c0
Sakin790 <sakinislam79@gmail.com>
2024-04-12 10:10:52 +0600

    api not complete

---
+++ src/api/signup/route.ts
@@ -0,0 +1,23 @@
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import { dbConnect } from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+//import { bcrypt } from "bcryptjs";
+import mongoose from "mongoose";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const { username, email, password } = await request.json();
+  } catch (error) {
+    console.error("Error registering Error", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error registering Error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
