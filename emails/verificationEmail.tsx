d023a8d93fe69d46e6d736a2d4e427f781874def
Sakin790 <sakinislam79@gmail.com>
2024-04-12 09:22:52 +0600

    ext issue solve

---
+++ emails/verificationEmail.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+  Html,
+  Head,
+  Font,
+  Preview,
+  Heading,
+  Row,
+  Section,
+  Text,
+  Button,
+} from "@react-email/components";
+
+interface EmailTemplateProps {
+  username: string;
+  otp: string;
+}
+
+const VerificationEmail: React.FC<EmailTemplateProps> = ({ username, otp }) => {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Verification Code</title>
+  
+      </Head>
+      <Preview>Verification Code: {otp}</Preview>
+
+      <Section>
+        <Row>
+          <Heading as="h2">Hello {username}</Heading>
+        </Row>
+        <Row>
+          <Text>
+            Thank you for signing up for [Your App Name]. To verify your account
+            and access all its features, please use the following one-time
+            password (OTP):
+          </Text>
+        </Row>
+        <Row>
+          <Text>{otp}</Text>
+        </Row>
+        <Row>
+          <Text>
+            This OTP is valid for a few minutes. For your security, please do
+            not share this code with anyone.
+          </Text>
+        </Row>
+      </Section>
+    </Html>
+  );
+};
+
+export default VerificationEmail;
---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,2 @@
+import { resend } from "@/lib/resend";
+
